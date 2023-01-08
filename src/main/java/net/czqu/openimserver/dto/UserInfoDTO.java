0316884ff488a166cb5297b222d7890a38e8ea36
czqu <117530179+czqu@users.noreply.github.com>
2023-01-08 15:10:09 +0800

    数据库测试接口

---
+++ src/main/java/net/czqu/openimserver/dto/UserInfoDTO.java
@@ -0,0 +1,63 @@
+package net.czqu.openimserver.dto;
+
+
+import java.util.Date;
+
+/**
+ * @program: Open-IM-Server-In-Java
+ * @description:
+ * @author: czq
+ * @create: 2023-01-08 14:27
+ **/
+public class UserInfoDTO {
+    private String userId;
+    private String name;
+    private String phone;
+    private String email;
+    private Date birth;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public UserInfoDTO setUserId(String userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UserInfoDTO setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public UserInfoDTO setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserInfoDTO setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public UserInfoDTO setBirth(Date birth) {
+        this.birth = birth;
+        return this;
+    }
+}
