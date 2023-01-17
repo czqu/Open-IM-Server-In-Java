b654c22f52d364a89cdc0e6577fd7aee2c083089
chatelier <cleverj@mail.ustc.edu.cn>
2023-01-17 21:03:02 +0800

    swagger-ui处理

--- src/main/java/net/czqu/openimserver/controller/UsersController.java
+++ src/main/java/net/czqu/openimserver/controller/UsersController.java
@@ -1,5 +1,9 @@
 package net.czqu.openimserver.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import net.czqu.openimserver.dto.user.AccountCheckListDTO;
 import net.czqu.openimserver.dto.user.AccountStatusDTO;
 import net.czqu.openimserver.dto.user.UserInfoDTO;
@@ -18,6 +22,7 @@ import java.util.List;
  * @author: czq
  * @create: 2023-01-08 14:46
  **/
+@Api(tags = "用户接口")
 @RestController
 @RequestMapping("/user")
 public class UsersController {
@@ -29,12 +34,13 @@ public class UsersController {
     }
 
     @GetMapping
-    public ResponseResult<UserInfoDTO> getUserInfo(@RequestParam(value = "id") String userId) throws UserException {
+    public ResponseResult<UserInfoDTO> getUserInfo(@RequestParam(value = "id") @Parameter(description= "EEEE")String userId) throws UserException {
         return ResponseResult.success(userService.getUserInfo(userId));
     }
+    @Operation(summary = "CCCCCC")
 
     @PostMapping ("/account_check")
-    public ResponseResult<List<AccountStatusDTO>> checkAccountStatus(@RequestBody AccountCheckListDTO body) {
+    public ResponseResult<List<AccountStatusDTO>> checkAccountStatus(@RequestBody AccountCheckListDTO body) throws UserException {
         return ResponseResult.success(userService.checkAccountStatus(body));
     }
 
